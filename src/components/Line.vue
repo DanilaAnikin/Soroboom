c477864d9a44b13f500ab0552f2cfbc3561badbd
Danila Anikin <danila.s.anikin@gmail.com>
2024-04-29 22:00:14 +0200

    MobileFInished

--- src/components/Line.vue
+++ src/components/Line.vue
@@ -8,7 +8,4 @@ import LinePage from '../pages/LinePage.vue';
       <LinePage />
     </div>
   </div>
-  <div class="absolute z-[-1] mt-[-100px]">
-    <img class="sm:flex hidden" src="../assets/Line.png">
-  </div>
 </template>
--- src/pages/LinePage.vue
+++ src/pages/LinePage.vue
@@ -3,8 +3,20 @@ import Phone from '../icons/Phone.vue';
 </script>
 
 <template>
-  <div class="flex justify-between sm:w-8/12 w-full sm:flex-row flex-col items-center">
+  <div class="flex justify-between sm:hidden w-full sm:flex-row flex-col items-center">
     <div class="sm:hidden w-full flex items-center flex-col gap-10 py-6 bg-gradient-to-b from-[#065803] from-[36.33%] to-[#27D45D] to-[100%]">
+      <img src="../assets/SoroboomLogo.png" >
+      <div class="flex text-white items-center gap-4 p-4 rounded-3xl bg-[#EC6A1F] h-fit">
+        <Phone :size="14" color="white" />
+        <div class="border-l border-white h-2"></div>
+        <div class="flex gap-1 items-center">
+          <span class="font-montserrat text-sm font-semibold">ЗАКАЗАТЬ ЗВОНОК</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="sm:flex hidden w-full justify-center items-center py-12 bg-gradient-to-l from-[#27D45D] from-[7.98%] to-[#065803] to-[75.62%]">
+    <div class="w-8/12 flex justify-between items-center">
       <img src="../assets/SoroboomLogo.png" >
       <div class="flex text-white items-center gap-4 p-4 rounded-3xl bg-[#EC6A1F] h-fit">
         <Phone :size="14" color="white" />
